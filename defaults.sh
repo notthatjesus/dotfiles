d8c6766e866172ce826ece64e36b2f60dbae60dd
Jesus Rodriguez <jesus.rodriguez.estevez@gmail.com>
2026-05-05 21:18:40 +0200

    Added waybar and omarchy-launch-webapp dotfiles as well as a defaults scripts

---
+++ defaults.sh
@@ -0,0 +1,8 @@
+#/usr/bin/bash
+
+# This scripts sets some defaults on new installations. Anything that can't be done with a file directly
+# ver: 2026-05-05
+#
+
+xdg-settings set default-web-browser firefox.desktop
+
